26b690d00b18fd3d7e0b0e7913b04748c51296fb
Sakin790 <sakinislam79@gmail.com>
2024-05-14 20:42:01 +0600

    working

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="m-0 p-0 w-screen h-screen box-border text-white bg-gray-800">
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="nav w-full h-[59px] bg-white flex justify-between bg-top">
+      <a href=""
+        class="font-bold text-xl items-center flex p-2 text-black hover:border-b border-green-500 transition duration-200 ease-in-out">VueX</a>
+      <div class="text-black flex font-[14px] p-3 gap-3 items-center">
+        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">Guide</a>
+        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">API Referance</a>
+        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">Release Note</a>
+        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">v4.x</a>
+
+        <div class="language-section relative" @mouseleave="isDropdownOpen = false">
+          <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">Language</a>
+          <ul v-show="isDropdownOpen"
+            class="dropdown absolute top-full left-0 bg-white shadow-md rounded overflow-hidden">
+            <li><a href="#">English</a></li>
+            <li><a href="#">Spanish</a></li>
+            <li><a href="#">French</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
