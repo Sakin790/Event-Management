8b61f852fadac5d5ac0c671edabf560ad69afc8d
Sakin790 <sakinislam79@gmail.com>
2024-05-20 20:13:43 +0600

    all done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <div class="m-0 p-0 w-screen h-screen box-border text-white bg-gray-800">
+  <div class="flex justify-center text-4xl font-extrabold p-3">
+    <h1>Find Your IP</h1>
+  </div>
 
-    <div class="nav w-full h-[59px] bg-white flex justify-between bg-top">
-      <a href=""
-        class="font-bold text-xl items-center flex p-2 text-black hover:border-b border-green-500 transition duration-200 ease-in-out">VueX</a>
-      <div class="text-black flex font-[14px] p-3 gap-3 items-center">
-        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">Guide</a>
-        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">API Referance</a>
-        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">Release Note</a>
-        <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">v4.x</a>
+  <div>
+    <form class="flex flex-col space-y-2 w-1/2 mx-auto" @submit.prevent="handleSubmit">
+      <label for="ip" class="text-sm font-medium text-gray-700">Your IP:</label>
+      <div class="flex items-center">
+        <input v-model="ip" type="text" id="ip" name="ip" required
+          class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500" />
+        <button type="submit"
+          class="ml-2 py-2 px-3 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">
+          Submit
+        </button>
+      </div>
+      <button type="button" @click="fetchApi"
+        class="mt-2 py-2 px-3 bg-blue-500 text-white font-medium rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">
+        Find
+      </button>
+    </form>
+  </div>
 
-        <div class="language-section relative" @mouseleave="isDropdownOpen = false">
-          <a href="" class="hover:border-b border-green-500 transition duration-200 ease-in-out">Language</a>
-          <ul v-show="isDropdownOpen"
-            class="dropdown absolute top-full left-0 bg-white shadow-md rounded overflow-hidden">
-            <li><a href="#">English</a></li>
-            <li><a href="#">Spanish</a></li>
-            <li><a href="#">French</a></li>
-          </ul>
+  <div>
+    <div class="container flex justify-center flex-col-1 mx-auto px-4 py-8">
+      <div v-if="loading" class="text-center text-xl font-bold">Loading...</div>
+      <div v-else>
+        <div v-if="apiResponse" class="flex flex-col space-y-4">
+          <div class="text-xl font-bold">
+            IP Address: <span class="text-blue-500">{{ apiResponse.query }}</span>
+          </div>
+          <div>Country Code: <span class="font-medium">{{ apiResponse.countryCode }}</span></div>
+          <div>Country Name: <span class="font-medium">{{ apiResponse.country }}</span></div>
+          <div>City: <span class="font-medium">{{ apiResponse.city }}</span></div>
+          <div>Latitude: <span class="font-medium">{{ apiResponse.lat }}</span></div>
+          <div>Longitude: <span class="font-medium">{{ apiResponse.lon }}</span></div>
+        </div>
+        <div v-if="apiResponse" class="flex flex-col space-y-2 mt-4">
+          <div class="text-xl font-bold">Connection</div>
+          <div>ASN: <span class="font-medium">{{ apiResponse.as }}</span></div>
+          <div>Organization: <span class="font-medium">{{ apiResponse.org }}</span></div>
+          <div>ISP: <span class="font-medium">{{ apiResponse.isp }}</span></div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      ip: "",
+      apiResponse: null,
+      error: "",
+      loading: false,
+    };
+  },
+  methods: {
+    handleSubmit(event) {
+      console.log("Form submitted:", this.ip, event);
+      event.target.reset();
+    },
+    async fetchApi() {
+      this.loading = true;
+      this.apiResponse = null;
+      this.error = "";
+      const url = `http://ip-api.com/json/${this.ip}`;
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("API data:", data);
+        this.apiResponse = data;
+        this.ip = data.query;
+      } catch (error) {
+        this.error = error;
+        console.error("Error fetching API:", error);
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+};
+</script>
---
+++ src/Store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex"
+
+export const store = createStore({
+
+    state: {
+        Loading: false,
+        Data: null
+
+    }
+
+})
